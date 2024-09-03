fcd96bd6b023ed757be4260cdd49668d2bde5919
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-09-03 08:14:11 +0300

    lab completion

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word.lower())
+
+    def match(self, possible_anagrams):
+        matches = []
+        for anagram in possible_anagrams:
+            if sorted(anagram.lower()) == self.sorted_word:
+                matches.append(anagram)
+        return matches
